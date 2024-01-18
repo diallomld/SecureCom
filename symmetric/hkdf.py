02cfa3d5d64a7f57c8edb3ffecf416e47a8ec509
diallomld <diallomld5@gmail.com>
2024-01-18 12:24:42 +0100

    last update

--- symmetric/hkdf.py
+++ symmetric/hkdf.py
@@ -1,6 +1,13 @@
 
-import os
+import os, sys
 from . import key_exchange as sharedKey
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+# Get the parent directory by going one level up
+parent_dir = os.path.dirname(current_dir)
+# Add the parent directory to sys.path
+sys.path.append(parent_dir)
+
 import sha256
 
 chemin_repertoire_script = os.path.dirname(os.path.abspath(__file__))
